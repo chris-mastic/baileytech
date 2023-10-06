cff46dc31a9e539b67d790e0b2b9afe2179cf9fb
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-10-06 15:43:56 -0500

    remove unused imports

--- app/main/views.py
+++ app/main/views.py
@@ -5,19 +5,7 @@ main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/', methods=['GET'])
 def home():
-    print(f"testing...another comment")
+    print(f"testing...another comment after adding new flask app")
     return render_template('index.html')
 
 
-
-@main.route('/update_server', methods=['POST'])
-def webhook():
-    print('In update_server')
-    if request.method == 'POST':
-        repo = git.Repo('https://github.com/chris-mastic/baileytech.git')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated Pythonanywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-    
