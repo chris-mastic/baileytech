35bb2644342387f4b2955fe21ad3448e40afbd9d
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-10-06 13:15:58 -0500

    test webhook

--- app/main/views.py
+++ app/main/views.py
@@ -13,31 +13,13 @@ def home():
 
 
 
-@main.route("/update", methods=["POST"])
-def update():
-    secret = '12345'
-    # Verify the signature of the webhook request
-    signature = request.headers.get("X-Hub-Signature")
-    digest = hmac.new(secret, request.data, "sha1").hexdigest()
-    if not hmac.compare_digest(signature, "sha1=" + digest):
-        return "Invalid signature", 403
+@main.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/chris-mastic/baileytech.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated Pythonanywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
     
-    # Pull the latest code from GitHub
-    subprocess.run(["git", "pull", "origin", "main"])
-    
-    # Reload the web app
-    subprocess.run(["touch", "/var/www/yourusername_pythonanywhere_com_wsgi.py"])
-
-    @main.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('https://github.com/chris-mastic/baileytech.git')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated Pythonanywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
-    
-
-    
-    return "OK", 200
