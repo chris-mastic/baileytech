d52719955593ade638499016e12374917a8dd8b8
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-11-27 11:40:07 -0600

    remove recaptcha import

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template
-from flask_recaptcha import ReCaptcha
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,8 +10,7 @@ def create_app():
         SECRET_KEY=os.getenv("FLASK_SECRET_KEY") or 'prc9FWjeLYh_KsPGm0vJcg',
     )
     app.config['RECAPTCHA_SECRET_KEY'] ='6LdJYR4pAAAAAOmQzWwtH5MHj0HCj8AXcaBsesgJ'
-    recaptcha = ReCaptcha(app)
-
+    
     from app.main.views import main
     
    
