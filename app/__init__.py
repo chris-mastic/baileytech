3cf8b745f72a691107347124ab5f1e21dccc3319
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-11-28 16:07:28 -0600

    remove refs to reCaptcha for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template
-from flask_google_recaptcha import GoogleReCaptcha
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 def create_app():
     app = Flask(__name__)
