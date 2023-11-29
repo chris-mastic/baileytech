d2d690ab04ecd2fe148e91d36ac9ffdadd5de769
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-11-29 15:39:58 -0600

    add base.html and child html files

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,42 @@
 import os
 from flask import Flask, render_template
-from flask_google_recaptcha import GoogleReCaptcha
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def create_app():
-    app = Flask(__name__)
-    recaptcha = GoogleReCaptcha(app=app)
-    app.config.from_mapping(
-        SECRET_KEY=os.getenv("FLASK_SECRET_KEY") or 'prc9FWjeLYh_KsPGm0vJcg',
-        RECAPTCHA_SITE_KEY='6LdJYR4pAAAAAOmQzWwtH5MHj0HCj8AXcaBsesgJ',
-        RECAPTCHA_SECRET_KEY='6LdJYR4pAAAAAFOaeMvNbjZpcJWPjyps5oiVBIuG',
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+from app.main.views import main
+app.register_blueprint(main)
+
+# def create_app():
+#     app = Flask(__name__)
+    
+#     app.config.from_mapping(
+#         SECRET_KEY=os.getenv("FLASK_SECRET_KEY") or 'prc9FWjeLYh_KsPGm0vJcg',
+#         RECAPTCHA_SITE_KEY='6LdJYR4pAAAAAOmQzWwtH5MHj0HCj8AXcaBsesgJ',
+#         RECAPTCHA_PRIVATE_KEY='6LdJYR4pAAAAAFOaeMvNbjZpcJWPjyps5oiVBIuG',
         
-    )
+#     )
     
     
-    from app.main.views import main
+#     from app.main.views import main
     
    
-    app.register_blueprint(main)
+#     app.register_blueprint(main)
 
 
 
 
-    return app
+#     return app
 
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,39 @@
 import json
-from flask import Blueprint, render_template, request
-from flask_google_recaptcha import GoogleReCaptcha
+from jinja2 import Template
+from flask import Blueprint, render_template, flash, redirect, url_for
 import git
+from app.form import CommentForm
 
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-@main.route('/', methods=['GET'])
-def home():
-    print(f"testing...another test. Yay!!")
-    pub_key = "6LdJYR4pAAAAAOmQzWwtH5MHj0HCj8AXcaBsesgJ"
-    return render_template('index.html',pub_key=pub_key)
+# @main.route('/', methods=['GET'])
+# def home():
+#     print(f"testing...another test. Yay!!")
+#     pub_key = "6LdJYR4pAAAAAOmQzWwtH5MHj0HCj8AXcaBsesgJ"
+#     return render_template('index.html',pub_key=pub_key)
 
-@main.route('/validate', methods=['POST', 'GET'])
-def validate():
-    print('in validate view')
-    # if recaptcha.verify():
-    #     pass
-    # else:
-    #     pass
-    return json.dumps({"abc": True})
+# @main.route('/validate', methods=['POST', 'GET'])
+# def validate():
+#     print('in validate view')
+#     # if recaptcha.verify():
+#     #     pass
+#     # else:
+#     #     pass
+#     return json.dumps({"abc": True})
+
+@main.route('/', methods = ['GET', 'POST'])
+@main.route('/baileytech', methods = ['GET', 'POST'])
+def comments():
+    print("in comments")
+    form = CommentForm()
+    print(form)
+    if form.validate_on_submit():
+        uusername = form.username.data
+        email = form.email.data       
+        body = form.comment.data
+        print('in form.validate')
+        flash('Your comment is now live!')  
+        return redirect(url_for('baileytech'))  
+    print('s/b rendering template')
+    return render_template('baileytech.html', title = 'Comment', form = form)
