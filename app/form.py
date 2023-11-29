ce05c50605f27da732240c330b7064d03867a27d
Christopher.Mazza <Christopher.Mazza@orleansasssessors.com>
2023-11-29 16:19:05 -0600

    start addding bootstrap classes back in

--- app/form.py
+++ app/form.py
@@ -3,7 +3,8 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class CommentForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    comment = TextAreaField('Comments...', validators=[DataRequired()])
+    username = StringField('Enter Name', validators=[DataRequired()])
+    email = StringField('Enter Email', validators=[DataRequired(), Email()])
+    phone = StringField('Enter Phone', validators=[DataRequired()])
+    interest = StringField('Interested In', validators=[DataRequired()])
     submit = SubmitField('Post')
